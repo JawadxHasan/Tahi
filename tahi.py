afac5f78088ce84a92eabe3f27235f30e17b4e95
JawadxHasan <63153456+JawadxHasan@users.noreply.github.com>
2024-12-29 20:37:00 +0600

    Add files via upload

---
+++ tahi.py
@@ -0,0 +1,10 @@
+import streamlit as st 
+st.title(':rainbow[You are the best thing that happened to me this year] :duck:')
+st.write("My dear Tahia, I love you the most. Time flies like a jet when I am around you. This year may didn't start with you, but I am glad that it ended with you.")
+st.write('I will hold your hands forever. Let\'s grow together and chase dreams. I want to see the world with you.')
+
+st.divider()
+if st.button('Click here'):
+    st.balloons()
+    st.write('I love you Tahia, you are my world!')
+    st.write('-from Shayer 😊')
